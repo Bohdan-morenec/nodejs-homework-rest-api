b045147d61db30964d85245e21466bc07a7d40b5
Fane <morenecba@gmail.com>
2021-09-26 16:10:03 +0300

    переместил функцию renameOriginalName выше

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,27 +1,27 @@
-const multer = require('multer')
-const path = require('path')
+const multer = require("multer");
+const path = require("path");
 
-const tempDir = path.join(__dirname, '../temp')
+const tempDir = path.join(__dirname, "../temp");
+
+const renameOriginalname = (name) => {
+  const [a, b] = name.split(".");
+  return `${new Date().getTime()}.${b}`;
+};
 
 const multerConfig = multer.diskStorage({
   destination: (_, __, cd) => {
-    cd(null, tempDir)
+    cd(null, tempDir);
   },
   filename: (_, file, cd) => {
-    cd(null, renameOriginalname(file.originalname))
+    cd(null, renameOriginalname(file.originalname));
   },
   limits: {
     fileSize: 1024,
   },
-})
-
-const renameOriginalname = (name) => {
-  const [a, b] = name.split('.')
-  return `${new Date().getTime()}.${b}`
-}
+});
 
 const upload = multer({
   storage: multerConfig,
-})
+});
 
-module.exports = upload
+module.exports = upload;
